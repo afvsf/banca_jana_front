36437d5d328b3e0e0537b87518bf4f18fac961a1
afvsf <145919614+afvsf@users.noreply.github.com>
2026-05-07 13:40:34 -0300

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,57 @@
+const API =
+'https://SEU-BACKEND.up.railway.app';
+
+
+async function carregarDashboard(){
+
+    const req = await fetch(
+        `${API}/dashboard`
+    );
+
+    const dados = await req.json();
+
+    document.getElementById(
+        'recebido'
+    ).innerHTML =
+        `R$ ${dados.total_recebido}`;
+
+    document.getElementById(
+        'pendente'
+    ).innerHTML =
+        `R$ ${dados.total_pendente}`;
+
+    document.getElementById(
+        'alunos'
+    ).innerHTML =
+        dados.total_alunos;
+
+    document.getElementById(
+        'inadimplentes'
+    ).innerHTML =
+        dados.inadimplentes;
+
+    let html = '';
+
+    dados.ultimos_pagamentos.forEach(item => {
+
+        html += `
+            <div class="border-bottom py-2">
+
+                <strong>${item.nome}</strong>
+
+                <br>
+
+                R$ ${item.valor_pago}
+
+            </div>
+        `;
+
+    });
+
+    document.getElementById(
+        'ultimosPagamentos'
+    ).innerHTML = html;
+
+}
+
+carregarDashboard();
